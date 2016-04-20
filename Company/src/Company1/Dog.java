1b3680a50c972e4d5155dafcf8ccbef8a2517978
Durgapr91 <lenovo@Lenovo-PC.station>
2016-04-20 17:00:03 +1200

    Testing first Git

---
+++ Company/src/Company1/Dog.java
@@ -0,0 +1,33 @@
+package Company1;
+
+public class Dog {
+	
+	 int age=7;
+	 
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Dog()
+	{
+		this.age=1;
+	}
+	public Dog(int age)
+	{
+		
+		this.age=age;
+		System.out.println(age);
+		
+	}
+	
+	public static void main(String []args)
+	{
+		Dog t=new Dog(10);
+		Dog t1=new Dog();
+		System.out.println(t.getAge());
+		System.out.println(t1.getAge());
+		
+	}
+}
